26dbdff9901a6d7bde2afbadcc417bb5839c2f03
KimSuhong <ksum0310@naver.com>
2023-03-27 00:39:27 +0900

    로그인 Response에 user_id 추가

--- users/views.py
+++ users/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.models import User
 from rest_framework import generics, status
 from rest_framework.response import Response
+from django.contrib.auth import get_user_model, authenticate
 
 from .serializers import RegisterSerializer, LoginSerializer, ProfileSerializer
 from .models import Profile
 
+User = get_user_model()
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
@@ -15,10 +17,13 @@ class LoginView(generics.GenericAPIView):
     serializer_class = LoginSerializer
 
     def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         token = serializer.validated_data
-        return Response({"token": token.key}, status=status.HTTP_200_OK)
+        return Response({"user_id": user.pk, "token": token.key}, status=status.HTTP_200_OK)
 
 
 class ProfileView(generics.RetrieveUpdateAPIView):

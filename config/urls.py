838fa79b418cf838134503de6db6d63f127ec0e6
KimSuhong <ksum0310@naver.com>
2023-05-15 02:36:37 +0900

    소셜 로그인 기능 설정 추가

--- config/urls.py
+++ config/urls.py
@@ -1,21 +1,5 @@
-"""config URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include, re_path
-from django.views.static import serve
 
 from .settings import MEDIA_ROOT, MEDIA_URL
 from django.conf.urls.static import static
@@ -38,13 +22,6 @@ urlpatterns = [  # BaseURL 다음 입력할 수 있는 url
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('stores/', include('stores.urls')),
-    # # social
-    # path('account/', include('rest_auth.urls')),
-    # path('account/registration/', include('rest_auth.registration.urls')),
-    # path('account/', include('allauth.urls')),
-    # url(r'account/registration/confirm-email/(?P<key>.+)/$', confirm_email, name='confirm_email'),
-    # path('', include('django.contrib.auth.urls')),
-    # re_path(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
 ]
 urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)  # 미디어 경로
 

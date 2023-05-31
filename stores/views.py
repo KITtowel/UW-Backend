f8ca2a111f56657c5f41e83c03ac13a99655cdd7
KimSuhong <ksum0310@naver.com>
2023-05-31 10:37:32 +0900

    가맹점 리스트 반환 중복코드 간소화

--- stores/views.py
+++ stores/views.py
@@ -49,21 +49,33 @@ class StoreListView(APIView):
             map_user = profile.user
             store = store.filter(store_address__contains=map_user.location)
 
-        serializer = StoreListSerializer(store, many=True,
-                                         context={'user_latitude': user_latitude, 'user_longitude': user_longitude})
+        map_type = request.resolver_match.url_name
+        if map_type == 'map_mark':
+            serializer = MapMarkSerializer(store, many=True,
+                                           context={'user_latitude': user_latitude, 'user_longitude': user_longitude})
+            sorted_data = sorted(serializer.data, key=lambda x: (x['distance']))
+            count = len(sorted_data)
+            response_data = {
+                'count': count,
+                'data': sorted_data
+            }
+            return Response(data=response_data, status=status.HTTP_200_OK)
+        else:
+            serializer = StoreListSerializer(store, many=True,
+                                             context={'user_latitude': user_latitude, 'user_longitude': user_longitude})
 
-        order_by = request.resolver_match.url_name
-        if order_by == 'distance_list':  # 거리가 가까운 순으로 정렬, 거리가 같으면 가맹점 이름순으로 정렬
-            sorted_data = sorted(serializer.data, key=lambda x: (x['distance'], x['store_name']))
-        elif order_by == 'like_list':  # 좋아요 개수가 많은 순으로 정렬, 좋아요 개수가 같으면 거리순 정렬, 거리가 같으면 가맹점 이름순으로 정렬
-            sorted_data = sorted(serializer.data, key=lambda x: (-x['likes_count'], x['distance'], x['store_name']))
-        elif order_by == 'rating_list':  # 평균 평점이 높은 순으로 정렬, 평균 평점이 같으면 거리순 정렬, 거리가 같으면 가맹점 이름순으로 정렬
-            sorted_data = sorted(serializer.data, key=lambda x: (-x['rating_mean'], x['distance'], x['store_name']))
+            order_by = request.resolver_match.url_name
+            if order_by == 'distance_list':  # 거리가 가까운 순으로 정렬, 거리가 같으면 가맹점 이름순으로 정렬
+                sorted_data = sorted(serializer.data, key=lambda x: (x['distance'], x['store_name']))
+            elif order_by == 'like_list':  # 좋아요 개수가 많은 순으로 정렬, 좋아요 개수가 같으면 거리순 정렬, 거리가 같으면 가맹점 이름순으로 정렬
+                sorted_data = sorted(serializer.data, key=lambda x: (-x['likes_count'], x['distance'], x['store_name']))
+            elif order_by == 'rating_list':  # 평균 평점이 높은 순으로 정렬, 평균 평점이 같으면 거리순 정렬, 거리가 같으면 가맹점 이름순으로 정렬
+                sorted_data = sorted(serializer.data, key=lambda x: (-x['rating_mean'], x['distance'], x['store_name']))
 
-        paginator = self.pagination_class()
-        result_page = paginator.paginate_queryset(sorted_data, request)
+            paginator = self.pagination_class()
+            result_page = paginator.paginate_queryset(sorted_data, request)
 
-        return paginator.get_paginated_response(result_page)
+            return paginator.get_paginated_response(result_page)
 
 
 # 가맹점 좋아요 기능
@@ -256,21 +268,33 @@ class CategoryListView(APIView):
             map_user = profile.user
             store = store.filter(store_address__contains=map_user.location)
 
-        serializer = StoreListSerializer(store, many=True,
-                                         context={'user_latitude': user_latitude, 'user_longitude': user_longitude})
+        map_type = request.resolver_match.url_name
+        if map_type == 'category_map_mark':
+            serializer = MapMarkSerializer(store, many=True,
+                                           context={'user_latitude': user_latitude, 'user_longitude': user_longitude})
+            sorted_data = sorted(serializer.data, key=lambda x: (x['distance']))
+            count = len(sorted_data)
+            response_data = {
+                'count': count,
+                'data': sorted_data
+            }
+            return Response(data=response_data, status=status.HTTP_200_OK)
+        else:
+            serializer = StoreListSerializer(store, many=True,
+                                             context={'user_latitude': user_latitude, 'user_longitude': user_longitude})
 
-        order_by = request.resolver_match.url_name
-        if order_by == 'category_distance':  # 거리가 가까운 순으로 정렬, 거리가 같으면 가맹점 이름순으로 정렬
-            sorted_data = sorted(serializer.data, key=lambda x: (x['distance'], x['store_name']))
-        elif order_by == 'category_like':  # 좋아요 개수가 많은 순으로 정렬, 좋아요 개수가 같으면 거리순 정렬, 거리가 같으면 가맹점 이름순으로 정렬
-            sorted_data = sorted(serializer.data, key=lambda x: (-x['likes_count'], x['distance'], x['store_name']))
-        elif order_by == 'category_rating':  # 평균 평점이 높은 순으로 정렬, 평균 평점이 같으면 거리순 정렬, 거리가 같으면 가맹점 이름순으로 정렬
-            sorted_data = sorted(serializer.data, key=lambda x: (-x['rating_mean'], x['distance'], x['store_name']))
+            order_by = request.resolver_match.url_name
+            if order_by == 'category_distance':  # 거리가 가까운 순으로 정렬, 거리가 같으면 가맹점 이름순으로 정렬
+                sorted_data = sorted(serializer.data, key=lambda x: (x['distance'], x['store_name']))
+            elif order_by == 'category_like':  # 좋아요 개수가 많은 순으로 정렬, 좋아요 개수가 같으면 거리순 정렬, 거리가 같으면 가맹점 이름순으로 정렬
+                sorted_data = sorted(serializer.data, key=lambda x: (-x['likes_count'], x['distance'], x['store_name']))
+            elif order_by == 'category_rating':  # 평균 평점이 높은 순으로 정렬, 평균 평점이 같으면 거리순 정렬, 거리가 같으면 가맹점 이름순으로 정렬
+                sorted_data = sorted(serializer.data, key=lambda x: (-x['rating_mean'], x['distance'], x['store_name']))
 
-        paginator = self.pagination_class()
-        result_page = paginator.paginate_queryset(sorted_data, request)
+            paginator = self.pagination_class()
+            result_page = paginator.paginate_queryset(sorted_data, request)
 
-        return paginator.get_paginated_response(result_page)
+            return paginator.get_paginated_response(result_page)
 
 
 # 검색어 키워드별 가맹점 리스트 반환
@@ -319,151 +343,30 @@ class SearchListView(APIView):
             map_user = profile.user
             store = store.filter(store_address__contains=map_user.location)
 
-        serializer = StoreListSerializer(store, many=True,
-                                         context={'user_latitude': user_latitude, 'user_longitude': user_longitude})
+        map_type = request.resolver_match.url_name
+        if map_type == 'search_map_mark':
+            serializer = MapMarkSerializer(store, many=True,
+                                           context={'user_latitude': user_latitude, 'user_longitude': user_longitude})
+            sorted_data = sorted(serializer.data, key=lambda x: (x['distance']))
+            count = len(sorted_data)
+            response_data = {
+                'count': count,
+                'data': sorted_data
+            }
+            return Response(data=response_data, status=status.HTTP_200_OK)
+        else:
+            serializer = StoreListSerializer(store, many=True,
+                                             context={'user_latitude': user_latitude, 'user_longitude': user_longitude})
 
-        order_by = request.resolver_match.url_name
-        if order_by == 'search_distance':  # 거리가 가까운 순으로 정렬, 거리가 같으면 가맹점 이름순으로 정렬
-            sorted_data = sorted(serializer.data, key=lambda x: (x['distance'], x['store_name']))
-        elif order_by == 'search_like':  # 좋아요 개수가 많은 순으로 정렬, 좋아요 개수가 같으면 거리순 정렬, 거리가 같으면 가맹점 이름순으로 정렬
-            sorted_data = sorted(serializer.data, key=lambda x: (-x['likes_count'], x['distance'], x['store_name']))
-        elif order_by == 'search_rating':  # 평균 평점이 높은 순으로 정렬, 평균 평점이 같으면 거리순 정렬, 거리가 같으면 가맹점 이름순으로 정렬
-            sorted_data = sorted(serializer.data, key=lambda x: (-x['rating_mean'], x['distance'], x['store_name']))
+            order_by = request.resolver_match.url_name
+            if order_by == 'search_distance':  # 거리가 가까운 순으로 정렬, 거리가 같으면 가맹점 이름순으로 정렬
+                sorted_data = sorted(serializer.data, key=lambda x: (x['distance'], x['store_name']))
+            elif order_by == 'search_like':  # 좋아요 개수가 많은 순으로 정렬, 좋아요 개수가 같으면 거리순 정렬, 거리가 같으면 가맹점 이름순으로 정렬
+                sorted_data = sorted(serializer.data, key=lambda x: (-x['likes_count'], x['distance'], x['store_name']))
+            elif order_by == 'search_rating':  # 평균 평점이 높은 순으로 정렬, 평균 평점이 같으면 거리순 정렬, 거리가 같으면 가맹점 이름순으로 정렬
+                sorted_data = sorted(serializer.data, key=lambda x: (-x['rating_mean'], x['distance'], x['store_name']))
 
-        paginator = self.pagination_class()
-        result_page = paginator.paginate_queryset(sorted_data, request)
+            paginator = self.pagination_class()
+            result_page = paginator.paginate_queryset(sorted_data, request)
 
-        return paginator.get_paginated_response(result_page)
-
-
-# 마커 표시용 가맹점 리스트 반환
-class MapMarkView(APIView):
-    def post(self, request):
-        try:
-            user_latitude = float(request.data.get('latitude'))  # 중앙 위도
-            user_longitude = float(request.data.get('longitude'))  # 중앙 경도
-            ne_latitude = float(request.data.get('ne_latitude'))  # 북동 위도
-            ne_longitude = float(request.data.get('ne_longitude'))  # 북동 경도
-            sw_latitude = float(request.data.get('sw_latitude'))  # 남서 위도
-            sw_longitude = float(request.data.get('sw_longitude'))  # 남서 경도
-        except ValueError:
-            return Response(status=status.HTTP_400_BAD_REQUEST, data={'message': '잘못된 값이 전달되었습니다.'})
-
-        # 북동 좌표와 남서 좌표 안에 있는 가게들만 필터링
-        store = StoreDaegu.objects.filter(
-            latitude__gte=sw_latitude, latitude__lte=ne_latitude,
-            longitude__gte=sw_longitude, longitude__lte=ne_longitude,
-        )
-
-        # 인증된 사용자면 사용자의 location 정보 가져와서 같은 지역의 가맹점만 반환
-        if request and request.user.is_authenticated:
-            profile = Profile.objects.get(user=self.request.user)
-            map_user = profile.user
-            store = store.filter(store_address__contains=map_user.location)
-
-        serializer = MapMarkSerializer(store, many=True,
-                                       context={'user_latitude': user_latitude, 'user_longitude': user_longitude})
-        sorted_data = sorted(serializer.data, key=lambda x: (x['distance']))
-        count = len(sorted_data)
-        response_data = {
-            'count': count,
-            'data': sorted_data
-        }
-        return Response(data=response_data, status=status.HTTP_200_OK)
-
-
-# 카테고리별 마커 표시용 가맹점 리스트 반환
-class CategoryMapMarkView(APIView):
-    def post(self, request):
-        try:
-            user_latitude = float(request.data.get('latitude'))  # 중앙 위도
-            user_longitude = float(request.data.get('longitude'))  # 중앙 경도
-            ne_latitude = float(request.data.get('ne_latitude'))  # 북동 위도
-            ne_longitude = float(request.data.get('ne_longitude'))  # 북동 경도
-            sw_latitude = float(request.data.get('sw_latitude'))  # 남서 위도
-            sw_longitude = float(request.data.get('sw_longitude'))  # 남서 경도
-            select_category = str(request.data.get('category'))  # 사용자가 선택한 카테고리
-        except ValueError:
-            return Response(status=status.HTTP_400_BAD_REQUEST, data={'message': '잘못된 값이 전달되었습니다.'})
-
-        category_list = select_category.split()
-        category_set = Q(category__exact=category_list[0])
-        if len(category_list) > 1:
-            for category in category_list[1:]:
-                category_set.add(Q(category__exact=category), category_set.OR)
-
-        # 북동 좌표와 남서 좌표 안에 있는 가게들만 필터링, 사용자가 선택한 카테고리와 같은 카테고리의 가게만 필터링
-        store = StoreDaegu.objects.filter(
-            latitude__gte=sw_latitude, latitude__lte=ne_latitude,
-            longitude__gte=sw_longitude, longitude__lte=ne_longitude).filter(category_set)
-
-        # 인증된 사용자면 사용자의 location 정보 가져와서 같은 지역의 가맹점만 반환
-        if request and request.user.is_authenticated:
-            profile = Profile.objects.get(user=self.request.user)
-            map_user = profile.user
-            store = store.filter(store_address__contains=map_user.location)
-
-        serializer = MapMarkSerializer(store, many=True,
-                                       context={'user_latitude': user_latitude, 'user_longitude': user_longitude})
-        sorted_data = sorted(serializer.data, key=lambda x: (x['distance']))
-        count = len(sorted_data)
-        response_data = {
-            'count': count,
-            'data': sorted_data
-        }
-        return Response(data=response_data, status=status.HTTP_200_OK)
-
-
-# 검색어별 마커 표시용 가맹점 리스트 반환
-class SearchMapMarkView(APIView):
-    def post(self, request):
-        try:
-            user_latitude = float(request.data.get('latitude'))  # 중앙 위도
-            user_longitude = float(request.data.get('longitude'))  # 중앙 경도
-            ne_latitude = float(request.data.get('ne_latitude'))  # 북동 위도
-            ne_longitude = float(request.data.get('ne_longitude'))  # 북동 경도
-            sw_latitude = float(request.data.get('sw_latitude'))  # 남서 위도
-            sw_longitude = float(request.data.get('sw_longitude'))  # 남서 경도
-            search_type = str(request.data.get('search_type'))  # 검색어 타입 (메뉴, 가게명)
-            search = str(request.data.get('search'))  # 사용자가 입력한 검색어
-        except ValueError:
-            return Response(status=status.HTTP_400_BAD_REQUEST, data={'message': '잘못된 값이 전달되었습니다.'})
-
-        # 북동 좌표와 남서 좌표 안에 있는 가게들만 필터링, 검색어 기반으로 가맹점 이름이나 메뉴에 일치하는 값이 있는 가맹점 필터링
-        search = search.replace(" ", "").strip()
-
-        if search_type == "전체":
-            store = StoreDaegu.objects.annotate(
-                re_store_name=Replace('store_name', Value(" "), Value("")),
-                re_menu_name=Replace('menu', Value(" "), Value(""))
-            ).filter(latitude__gte=sw_latitude, latitude__lte=ne_latitude, longitude__gte=sw_longitude,
-                     longitude__lte=ne_longitude). \
-                filter(Q(re_store_name__contains=search) | Q(re_menu_name__contains=search))
-        elif search_type == "가게명":
-            store = StoreDaegu.objects.annotate(
-                re_store_name=Replace('store_name', Value(" "), Value(""))
-            ).filter(latitude__gte=sw_latitude, latitude__lte=ne_latitude, longitude__gte=sw_longitude,
-                     longitude__lte=ne_longitude, re_store_name__contains=search
-                     )
-        elif search_type == "메뉴":
-            store = StoreDaegu.objects.annotate(
-                re_menu=Replace('menu', Value(" "), Value(""))
-            ).filter(latitude__gte=sw_latitude, latitude__lte=ne_latitude, longitude__gte=sw_longitude,
-                     longitude__lte=ne_longitude, re_menu__contains=search
-                     )
-
-        # 인증된 사용자면 사용자의 location 정보 가져와서 같은 지역의 가맹점만 반환
-        if request and request.user.is_authenticated:
-            profile = Profile.objects.get(user=self.request.user)
-            map_user = profile.user
-            store = store.filter(store_address__contains=map_user.location)
-
-        serializer = MapMarkSerializer(store, many=True,
-                                       context={'user_latitude': user_latitude, 'user_longitude': user_longitude})
-        sorted_data = sorted(serializer.data, key=lambda x: (x['distance']))
-        count = len(sorted_data)
-        response_data = {
-            'count': count,
-            'data': sorted_data
-        }
-        return Response(data=response_data, status=status.HTTP_200_OK)
+            return paginator.get_paginated_response(result_page)
